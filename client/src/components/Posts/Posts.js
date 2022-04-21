fb13a2fe0bd444d92d08140177d285af2e509f27
Catalin Mihai <mishu_leu15@yahoo.com>
2022-04-21 22:38:28 +0300

    added code in form, posts and post component, installed axios and created Ajax requests, displayed posts in client based on the data f on the data from the database

--- client/src/components/Posts/Posts.js
+++ client/src/components/Posts/Posts.js
@@ -2,19 +2,27 @@ import React from 'react';
 import Post from './Post/Post';
 import usestyles from './styles';
 import { useSelector } from 'react-redux';
+import { Grid, CircularProgress } from '@material-ui/core';
 
 function Posts() {
   const classes = usestyles();
   const posts = useSelector((state) => state.posts);
 
-  console.log(posts);
-
-  return (
-    <>
-      <h1>Posts</h1>
-      <Post />
-      <Post />
-    </>
+  return !posts.length ? (
+    <CircularProgress />
+  ) : (
+    <Grid
+      className={classes.container}
+      container
+      alignItems='stretch'
+      spacing={3}
+    >
+      {posts.map((post) => (
+        <Grid key={post._id} item xs={12} sm={6}>
+          <Post post={post} />
+        </Grid>
+      ))}
+    </Grid>
   );
 }
 
--- client/src/index.js
+++ client/src/index.js
@@ -1,18 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
-import {
-  legacy_createStore as createStore,
-  applyMiddleware,
-  compose,
-} from 'redux';
+import { legacy_createStore as createStore, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
 import './index.css';
 import App from './App';
+import { composeWithDevTools } from 'redux-devtools-extension';
 
 import reducers from './reducers/index';
 
-const store = createStore(reducers, compose(applyMiddleware(thunk)));
+const store = createStore(
+  reducers,
+  composeWithDevTools(applyMiddleware(thunk))
+);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
